238e7d2d38ed4e9638ab726a9be6c17065badcb8
PankajSinghSheoran <pankajsheoran728@gmail.com>
2023-02-23 04:13:27 +0530

    Created navbar for homepage

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,35 +1,73 @@
-import React from "react";
-import { Breadcrumb, Layout, theme } from "antd";
+import React, { useState } from "react";
+import { Layout, Menu, MenuProps, theme } from "antd";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 
 const { Content, Footer } = Layout;
 
-const App: React.FC = () => {
+const items: MenuProps["items"] = [
+  {
+    label: <a href="/">Home</a>,
+    key: "mail",
+    icon: <MailOutlined />,
+  },
+  {
+    label: <a href="./alumni">Alumni</a>,
+    key: "app",
+    icon: <AppstoreOutlined />,
+    children: [
+      {
+        label: <a href="./alumni">Register Now</a>,
+        key: "1",
+      },
+      {
+        label: <a href="./alumni">Login</a>,
+        key: "2",
+      },
+    ],
+  },
+  {
+    label: <a href="./news">News</a>,
+    key: "SubMenu",
+    icon: <SettingOutlined />,
+  },
+  {
+    label: (
+      <a
+        href="https://www.crmjatcollege.com/"
+        target={"_blank"}
+        rel="noopener noreferrer"
+      >
+        Official website
+      </a>
+    ),
+    key: "alipay",
+  },
+];
+
+export default function Home() {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
 
+  const [current, setCurrent] = useState("mail");
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
   return (
     <Layout className="flex">
       <Content className="site-layout" style={{ padding: "0 50px" }}>
-        <Breadcrumb style={{ margin: "16px 0" }}>
-          <Breadcrumb.Item>
-            <button>
-              <a href="/"> Home </a>
-            </button>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            <button>
-              <a href="/contact"> Register </a>
-            </button>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            <a href="/alumni"> Alumni </a>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            <a href="/news"> News </a>
-          </Breadcrumb.Item>
-        </Breadcrumb>
-
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={items}
+        />
         <div
           style={{ padding: 24, minHeight: 380, background: colorBgContainer }}
         >
@@ -48,6 +86,4 @@ const App: React.FC = () => {
       </Footer>
     </Layout>
   );
-};
-
-export default App;
+}
